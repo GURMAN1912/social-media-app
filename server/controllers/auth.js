e82e614bb5a6474dd426487686d80f34fbca0495
GURMAN1912 <manansinghsambhi@gmail.com>
2024-02-23 22:31:08 +0530

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,54 @@
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+import User from '../models/user.js'
+
+export const register= async(req,res)=>{
+    try{
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        }=req.body;
+        const salt=await bcrypt.genSalt();
+        const passwordHash=await bcrypt.hash(password,salt);
+        const newUser= new User({
+            firstName,
+            lastName,
+            email,
+            password:passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile:Math.floor(Math.random()*1000),
+            impressions:Math.floor(Math.random()*1000),
+        })
+        const savedUser=await newUser.save()
+        res.status(201).json(savedUser)
+    }
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+
+}
+export const login =async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const foundUser=await User({email:email});
+        if(!foundUser) return res.status(400).json({msg: "User not found!..."})
+
+        const isMatch=await bcrypt.compare(password,foundUser.password)
+        if(!isMatch) return res.status(400).json({msg:"Wrong password..."})
+
+        const token=jwt.sign({id:foundUser.id},process.env.JWT_SECRET)
+        delete foundUser.password;
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json({foundUser })
+    } catch (error) {
+        res.status(500).json({error:err.message});       
+    }
+}
